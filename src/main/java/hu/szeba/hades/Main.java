3e4086389422febe49c2e086957fe97e15698a45
Szeba25 <lustmord20@gmail.com>
2018-08-31 19:21:32 +0200

    Changed some UI elements.

--- src/main/java/hu/szeba/hades/Main.java
+++ src/main/java/hu/szeba/hades/Main.java
@@ -7,7 +7,9 @@ import javax.swing.*;
 public class Main {
 
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(CampaignSelector::new);
+        SwingUtilities.invokeLater(() -> {
+            new CampaignSelector();
+        });
     }
 
 }
--- src/main/java/hu/szeba/hades/gui/CampaignSelector.java
+++ src/main/java/hu/szeba/hades/gui/CampaignSelector.java
@@ -1,18 +1,20 @@
 package hu.szeba.hades.gui;
 
 import javax.swing.*;
-import javax.swing.border.Border;
 import javax.swing.border.EmptyBorder;
+import javax.swing.border.EtchedBorder;
 import java.awt.*;
 
 public class CampaignSelector {
 
     private JFrame mainFrame;
 
-    private JPanel rightPanel;
+    private JPanel leftPanel;
 
     private JList campaignList;
     private JScrollPane campaignListScroller;
+
+    private JPanel rightPanel;
 
     private JTextArea descriptionArea;
     private JButton startButton;
@@ -25,35 +27,44 @@ public class CampaignSelector {
     private void create() {
         mainFrame = new JFrame();
         mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        mainFrame.setResizable(false);
-        mainFrame.setLayout(new BorderLayout());
+        mainFrame.setResizable(true);
+        mainFrame.setLayout(new BoxLayout(mainFrame.getContentPane(), BoxLayout.X_AXIS));
+        mainFrame.setTitle("Hades: Campaign selection");
+
+        leftPanel = new JPanel();
+        leftPanel.setLayout(new BorderLayout());
+        leftPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+        String[] campaigns = {"Hello world!", "Counting to 10"};
+        campaignList = new JList(campaigns);
+        campaignList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        campaignListScroller = new JScrollPane(campaignList);
+        campaignListScroller.setPreferredSize(new Dimension(200, 500));
+        campaignListScroller.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        campaignListScroller.setBorder(BorderFactory.createEtchedBorder());
+
+        leftPanel.add(campaignListScroller);
 
         rightPanel = new JPanel();
         rightPanel.setLayout(new BoxLayout(rightPanel, BoxLayout.Y_AXIS));
         rightPanel.setBorder(new EmptyBorder(5, 0, 5, 5));
 
-        String[] campaigns = {"Hello world!", "Counting to 10"};
-        campaignList = new JList(campaigns);
-        campaignListScroller = new JScrollPane(campaignList);
-        campaignListScroller.setPreferredSize(new Dimension(300, 500));
-        campaignListScroller.setBorder(new EmptyBorder(5, 5, 5, 5));
-
         descriptionArea = new JTextArea();
-        descriptionArea.setPreferredSize(new Dimension(500, 400));
+        descriptionArea.setPreferredSize(new Dimension(400, 500));
         descriptionArea.setAlignmentX(Component.CENTER_ALIGNMENT);
         descriptionArea.setEditable(false);
+        descriptionArea.setBorder(BorderFactory.createEtchedBorder());
 
-        startButton = new JButton();
-        startButton.setAlignmentX(Component.CENTER_ALIGNMENT);
-        startButton.setText("Start");
-        startButton.setMinimumSize(new Dimension(150, 30));
+        startButton = new JButton("Start");
+        startButton.setAlignmentX(Component.RIGHT_ALIGNMENT);
+        startButton.setFocusPainted(false);
 
         rightPanel.add(descriptionArea);
         rightPanel.add(Box.createRigidArea(new Dimension(0, 5)));
         rightPanel.add(startButton);
 
-        mainFrame.getContentPane().add(campaignListScroller, BorderLayout.WEST);
-        mainFrame.getContentPane().add(rightPanel, BorderLayout.EAST);
+        mainFrame.getContentPane().add(leftPanel);
+        mainFrame.getContentPane().add(rightPanel);
         mainFrame.pack();
     }
 
