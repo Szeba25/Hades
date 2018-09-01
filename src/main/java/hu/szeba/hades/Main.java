b59de24820aa5f66c46f3308aead62aa607a8e47
Szeba25 <lustmord20@gmail.com>
2018-09-01 12:55:18 +0200

    Removed system look and feel.

--- src/main/java/hu/szeba/hades/Main.java
+++ src/main/java/hu/szeba/hades/Main.java
@@ -8,17 +8,6 @@ public class Main {
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
-            try {
-                UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-            } catch (ClassNotFoundException e) {
-                e.printStackTrace();
-            } catch (InstantiationException e) {
-                e.printStackTrace();
-            } catch (IllegalAccessException e) {
-                e.printStackTrace();
-            } catch (UnsupportedLookAndFeelException e) {
-                e.printStackTrace();
-            }
             new CampaignSelector();
         });
     }
