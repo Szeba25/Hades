18d9def6f511f6fcaa8472556935ba2d2abcae5f
Szeba25 <lustmord20@gmail.com>
2018-09-06 20:10:52 +0200

    Passed options to tasks!

--- src/main/java/hu/szeba/hades/meta/Options.java
+++ src/main/java/hu/szeba/hades/meta/Options.java
@@ -4,24 +4,33 @@ import java.io.File;
 
 public class Options {
 
-    private File compilerPath;
+    private File compilerPath_C;
+    private File compilerPath_CPP;
     private File campaignDatabasePath;
     private File workingDirectoryPath;
 
-    public Options(File compilerPath, File campaignDatabasePath, File workingDirectoryPath) {
-        this.compilerPath = compilerPath;
+    public Options(File compilerPath_C,
+                   File compilerPath_CPP,
+                   File campaignDatabasePath,
+                   File workingDirectoryPath) {
+        this.compilerPath_C = compilerPath_C;
+        this.compilerPath_CPP = compilerPath_CPP;
         this.campaignDatabasePath = campaignDatabasePath;
         this.workingDirectoryPath = workingDirectoryPath;
     }
 
     public void checkPaths() {
-        System.out.println(compilerPath.exists());
+        System.out.println(compilerPath_C.exists());
         System.out.println(campaignDatabasePath.exists());
         System.out.println(workingDirectoryPath.exists());
     }
 
-    public File getCompilerPath() {
-        return compilerPath;
+    public File getCompilerPath_C() {
+        return compilerPath_C;
+    }
+
+    public File getCompilerPath_CPP() {
+        return compilerPath_CPP;
     }
 
     public File getCampaignDatabasePath() {
--- src/main/java/hu/szeba/hades/model/task/Task.java
+++ src/main/java/hu/szeba/hades/model/task/Task.java
@@ -1,18 +1,22 @@
 package hu.szeba.hades.model.task;
 
+import hu.szeba.hades.meta.Options;
+
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Task {
 
+    private Options options;
     private File taskDirectory;
     private File taskWorkingDirectory;
     private String taskName;
     private List<Solution> solutions;
     private List<SourceFile> sources;
 
-    public Task(File campaignDirectory, File campaignWorkingDirectory, String taskName) {
+    public Task(Options options, File campaignDirectory, File campaignWorkingDirectory, String taskName) {
+        this.options = options;
         taskDirectory = new File(campaignDirectory, taskName);
         taskWorkingDirectory = new File(campaignWorkingDirectory, taskName);
         this.taskName = taskName;
