112eb61fa604f5dcf91a9ea464c25fef5cac2479
Szeba25 <lustmord20@gmail.com>
2018-09-06 19:36:29 +0200

    Added task to task solving control.

--- src/main/java/hu/szeba/hades/control/task/TaskSolvingControl.java
+++ src/main/java/hu/szeba/hades/control/task/TaskSolvingControl.java
@@ -1,5 +1,13 @@
 package hu.szeba.hades.control.task;
 
+import hu.szeba.hades.model.task.Task;
+
 public class TaskSolvingControl {
+
+    private Task currentTask;
+
+    public TaskSolvingControl(Task currentTask) {
+        this.currentTask = currentTask;
+    }
 
 }
