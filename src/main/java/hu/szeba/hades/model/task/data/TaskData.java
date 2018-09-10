fc6ea92bd9b093b507e6fc5514f38110ee008a35
Szeba25 <lustmord20@gmail.com>
2018-09-10 23:41:10 +0200

    Added TaskData class instead of Task.

---
+++ src/main/java/hu/szeba/hades/model/task/data/TaskData.java
@@ -0,0 +1,28 @@
+package hu.szeba.hades.model.task.data;
+
+import hu.szeba.hades.model.task.Solution;
+import hu.szeba.hades.model.task.SourceFile;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TaskData {
+
+    private File taskDirectory;
+    private File taskWorkingDirectory;
+
+    private String taskName;
+
+    private List<Solution> solutions;
+    private List<SourceFile> sources;
+
+    public TaskData(File taskDirectory, File taskWorkingDirectory, String taskName) {
+        this.taskDirectory = taskDirectory;
+        this.taskWorkingDirectory = taskWorkingDirectory;
+        this.taskName = taskName;
+        solutions = new ArrayList<>();
+        sources = new ArrayList<>();
+    }
+
+}
