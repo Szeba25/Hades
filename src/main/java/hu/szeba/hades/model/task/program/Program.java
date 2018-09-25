721507c5aab87f42020dbc1b45008c2ea522239a
Szeba25 <lustmord20@gmail.com>
2018-09-26 00:40:06 +0200

    Added location to program.

--- src/main/java/hu/szeba/hades/model/task/program/Program.java
+++ src/main/java/hu/szeba/hades/model/task/program/Program.java
@@ -2,8 +2,10 @@ package hu.szeba.hades.model.task.program;
 
 import hu.szeba.hades.model.task.result.Result;
 
+import java.io.File;
+
 public interface Program {
 
-    Result run(ProgramInput input);
+    Result run(File location, ProgramInput input);
 
 }
--- src/main/java/hu/szeba/hades/model/task/program/ProgramC.java
+++ src/main/java/hu/szeba/hades/model/task/program/ProgramC.java
@@ -2,10 +2,12 @@ package hu.szeba.hades.model.task.program;
 
 import hu.szeba.hades.model.task.result.Result;
 
+import java.io.File;
+
 public class ProgramC implements Program {
 
     @Override
-    public Result run(ProgramInput input) {
+    public Result run(File location, ProgramInput input) {
         return new Result();
     }
 
