380977c0f594b21924dbaebb8c379bbef696b2c0
Szeba25 <lustmord20@gmail.com>
2018-09-25 21:50:50 +0200

    Formatting changes.

--- src/main/java/hu/szeba/hades/model/task/TaskFactoryDecider.java
+++ src/main/java/hu/szeba/hades/model/task/TaskFactoryDecider.java
@@ -3,12 +3,14 @@ package hu.szeba.hades.model.task;
 public class TaskFactoryDecider {
 
     public static TaskFactory decideFactory(String language) throws UnsupportedProgrammingLanguageException {
+
         switch(language) {
             case SupportedLanguages.C:
                 return new TaskCFactory();
             default:
                 throw new UnsupportedProgrammingLanguageException(language);
         }
+
     }
 
 }
