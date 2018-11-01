64d30a8685843415e60a3f48ff6a0c96021f1ab3
Szeba25 <lustmord20@gmail.com>
2018-11-01 14:25:41 +0100

    Optimized imports in main

--- src/main/java/hu/szeba/hades/Main.java
+++ src/main/java/hu/szeba/hades/Main.java
@@ -1,25 +1,13 @@
 package hu.szeba.hades;
 
-import hu.szeba.hades.io.StoryXMLFile;
-import hu.szeba.hades.io.TaskGraphFile;
-import hu.szeba.hades.model.compiler.ProgramCompiler;
-import hu.szeba.hades.model.compiler.ProgramCompilerC;
-import hu.szeba.hades.model.course.Course;
-import hu.szeba.hades.model.course.CourseDatabase;
-import hu.szeba.hades.model.task.data.SourceFile;
-import hu.szeba.hades.model.task.graph.AdjacencyMatrix;
-import hu.szeba.hades.view.campaign.TaskSelectorView;
 import hu.szeba.hades.meta.Options;
 import hu.szeba.hades.model.campaign.Campaign;
-import org.xml.sax.SAXException;
+import hu.szeba.hades.model.course.Course;
+import hu.szeba.hades.model.course.CourseDatabase;
+import hu.szeba.hades.view.campaign.TaskSelectorView;
 
 import javax.swing.*;
-import javax.xml.parsers.ParserConfigurationException;
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.FileSystems;
-import java.util.LinkedList;
-import java.util.List;
 
 public class Main {
 
