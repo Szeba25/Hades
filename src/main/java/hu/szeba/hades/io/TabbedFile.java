883c1ab2a31b628b3ed9f2de21d6faeed7feeab2
Szeba25 <lustmord20@gmail.com>
2018-11-20 12:01:35 +0100

    Writing to output stream works

---
+++ src/main/java/hu/szeba/hades/io/TabbedFile.java
@@ -0,0 +1,12 @@
+package hu.szeba.hades.io;
+
+import java.io.File;
+import java.io.IOException;
+
+public class TabbedFile extends DataFile {
+
+    public TabbedFile(File file) throws IOException {
+        super(file, "\t");
+    }
+
+}
--- src/main/java/hu/szeba/hades/model/task/program/ProgramInput.java
+++ src/main/java/hu/szeba/hades/model/task/program/ProgramInput.java
@@ -1,13 +1,20 @@
 package hu.szeba.hades.model.task.program;
 
+import hu.szeba.hades.io.TabbedFile;
+
 import java.io.File;
+import java.io.IOException;
 
 public class ProgramInput {
 
-    private File inputFilePath;
+    private TabbedFile file;
 
-    public ProgramInput(File inputFilePath) {
-        this.inputFilePath = inputFilePath;
+    public ProgramInput(File inputFilePath) throws IOException {
+        this.file = new TabbedFile(inputFilePath);
+    }
+
+    public TabbedFile getFile() {
+        return file;
     }
 
 }
