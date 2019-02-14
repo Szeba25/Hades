c4b4e16f35516b74347fe9204027bbb23ac428a7
Szeba25 <lustmord20@gmail.com>
2019-02-14 17:05:28 +0100

    Removed some unused imports

--- src/main/java/hu/szeba/hades/Main.java
+++ src/main/java/hu/szeba/hades/Main.java
@@ -1,15 +1,13 @@
 package hu.szeba.hades;
 
-import com.bulenkov.darcula.DarculaLaf;
 import hu.szeba.hades.meta.Options;
-import hu.szeba.hades.model.topic.Topic;
 import hu.szeba.hades.model.course.Course;
 import hu.szeba.hades.model.course.CourseDatabase;
+import hu.szeba.hades.model.topic.Topic;
 import hu.szeba.hades.view.topic.TaskSelectorView;
 import org.xml.sax.SAXException;
 
 import javax.swing.*;
-import javax.swing.plaf.basic.BasicLookAndFeel;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
 
