38c2625609c38d281d09242f62df65e08cb82e6e
Szeba25 <lustmord20@gmail.com>
2019-02-14 11:40:23 +0100

    Removed consolas font

--- src/main/java/hu/szeba/hades/view/task/TaskSolvingView.java
+++ src/main/java/hu/szeba/hades/view/task/TaskSolvingView.java
@@ -81,7 +81,7 @@ public class TaskSolvingView extends BaseView {
         topPanel = new JPanel();
         topPanel.setLayout(new BorderLayout());
 
-        monoFont = new Font("Consolas", Font.PLAIN, 16);
+        monoFont = new Font("Monospaced", Font.PLAIN, 16);
 
         fileListModel = new DefaultListModel<>();
         fileList = new JList<>(fileListModel);
