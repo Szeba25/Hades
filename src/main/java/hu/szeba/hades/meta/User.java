4d00015dbb07944baa0794f93d7f5f764612d61e
Szeba25 <lustmord20@gmail.com>
2019-02-23 17:01:45 +0100

    Added user working directory

--- src/main/java/hu/szeba/hades/meta/User.java
+++ src/main/java/hu/szeba/hades/meta/User.java
@@ -1,13 +1,23 @@
 package hu.szeba.hades.meta;
 
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+
 public class User {
 
     private String id;
     private String name;
+    private File userWorkingDirectory;
 
-    public User(String id, String name) {
+    public User(String id, String name) throws IOException {
         this.id = id;
         this.name = name;
+        userWorkingDirectory = new File(Options.getWorkingDirectoryPath(), id);
+        if (!userWorkingDirectory.exists()) {
+            FileUtils.forceMkdir(userWorkingDirectory);
+        }
     }
 
     public String getId() {
@@ -17,4 +27,9 @@ public class User {
     public String getName() {
         return name;
     }
+
+    public File getUserWorkingDirectory() {
+        return userWorkingDirectory;
+    }
+
 }
