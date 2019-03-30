c37d9e04af378cc28f9d14701040d616d4d6d2e1
Szeba25 <lustmord20@gmail.com>
2019-03-30 14:54:48 +0100

    Reworked descriptive element

--- src/main/java/hu/szeba/hades/wizard/view/elements/DescriptiveElement.java
+++ src/main/java/hu/szeba/hades/wizard/view/elements/DescriptiveElement.java
@@ -4,20 +4,13 @@ import hu.szeba.hades.main.view.elements.MappedElement;
 
 public class DescriptiveElement extends MappedElement {
 
-    private String display;
-
     public DescriptiveElement(String id, String title) {
         super(id, title);
-        this.display = id + ": " + title;
     }
 
     @Override
     public String toString() {
-        return display;
+        return getId() + ": " + getTitle();
     }
 
-    public void setTitle(String title) {
-        super.setTitle(title);
-        this.display = getId() + ": " + title;
-    }
 }
